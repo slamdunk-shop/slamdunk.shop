147ded435f1cd4587e6afd1be222a3d3b2b81fef
Andrey Dobrynin <andrdobrynin@gmail.com>
2018-06-04 00:18:28 +0700

    Delete plugins folders from git

--- wp-content/plugins/wowmall-shortcodes/inc/hooks.php
+++
@@ -1,6 +0,0 @@
-<?php
-add_action( 'wowmall_posts_carousel_item', 'wowmall_posts_carousel_item_thumb', 10 );
-
-add_action( 'wowmall_posts_carousel_item', 'wowmall_posts_carousel_item_date', 20 );
-
-add_action( 'wowmall_posts_carousel_item', 'wowmall_posts_carousel_item_title', 30 );
